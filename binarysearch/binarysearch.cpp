7c5f5177b28e46da4158add2df0cffb1d9858c40
firyalshafa <shafafiryal782@gmail.com>
2024-06-13 15:38:55 +0700

    add member metod prosdur insert

--- binarysearch/binarysearch.cpp
+++ binarysearch/binarysearch.cpp
@@ -26,6 +26,27 @@ public:
     binarytree() {
         ROOT = nullptr;//initializing root to null
     }
+    void insert(string element) { //insert a node in the binary search tree
+        node* newNode = new node(element, nullptr, nullptr);// alocate memory for the new node
+        newNode->info = element;// assgin value to the data field of the new node
+        newNode->leftchild = nullptr; // make the left child of the node point to null
+        newNode->rightchild = nullptr; // make the right child of the new node point to null
+
+        node* parent = nullptr;
+        node* currentNode = nullptr;
+        search(element parent, currentNode); // locate the node wich all be the parent of the tobe insert
+
+        if (parent == nullptr) {
+            ROOT = newNode; // mark the new node ass root
+            return;
+        }
+        if (element < parent->info) {// if the value in the data field of the new node is less than that  of the 
+            parent->leftchild = newNode;//make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) {// if the value in data field of the newnode
+            parent->rightchild = newNode;// make the right child of the parent point to the new node 
+        }
+    }
 };
 
 int main()
